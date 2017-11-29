be8aeb5c6dcc009ed8cde635329ebebf37c40ec9
Deville Matthew <kemiimek@live.fr>
2017-11-29 19:36:56 +0100

    Initial commit

--- srcs/ft_putsrc/ft_put_intmax_base_fd.c
+++ srcs/ft_putsrc/ft_put_intmax_base_fd.c
@@ -6,7 +6,7 @@
 /*   By: mdeville <mdeville@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/28 16:56:16 by mdeville          #+#    #+#             */
-/*   Updated: 2017/11/29 18:19:30 by mdeville         ###   ########.fr       */
+/*   Updated: 2017/11/29 19:36:19 by mdeville         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,12 @@ void		ft_put_intmax_base_fd(
 							const char *base,
 							t_token *token)
 {
-	int blen;
+	int		blen;
+	int		nbrlen;
+	char	*tmp;
 
 	blen = ft_strlen(base);
+	nbrlen = ft_nbrlen(nbr);
+	tmp = ft_itoamax(nbr);
 	
 }
